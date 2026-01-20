be91c4994346730504d22fa7c551220a3c122587
Aysel63 <aysel.abdulzade05@gmail.com>
2026-01-20 12:27:04 -0800

    feat: add role-based page access and display user authentication details

--- 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/luv2code/springboot/demosecurity/controller/DemoController.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/luv2code/springboot/demosecurity/controller/DemoController.java
@@ -10,4 +10,18 @@ public class DemoController {
     public String showForm() {
         return "home";
     }
+
+   // add a request mapping for /leaders
+
+    @GetMapping("/leaders")
+    public String showLeaders() {
+        return "leaders";
+    }
+
+    // add a request mapping for /systems
+    @GetMapping("/systems")
+    public String showSystems() {
+        return "systems";
+    }
+
 }
