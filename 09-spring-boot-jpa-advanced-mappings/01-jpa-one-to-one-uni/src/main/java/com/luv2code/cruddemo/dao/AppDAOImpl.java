ccce38dc6addb31aa11419375c9847329cc2f703
user <aysel.abdulzade05@gmail.com>
2026-01-27 20:39:07 +0400

    feat: Implement One-to-One Unidirectional mapping

---
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/dao/AppDAOImpl.java
@@ -0,0 +1,28 @@
+package com.luv2code.cruddemo.dao;
+
+import com.luv2code.cruddemo.entity.Instructor;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AppDAOImpl implements AppDAO{
+
+
+    // define field for entity manager
+    private EntityManager entityManager;
+
+    // inject entity manager using constructor injection
+    @Autowired
+    public AppDAOImpl(EntityManager entityManager){
+        this.entityManager=entityManager;
+    }
+
+    @Transactional
+    @Override
+    public void save(Instructor theInstructor) {
+        entityManager.persist(theInstructor);
+
+    }
+}
