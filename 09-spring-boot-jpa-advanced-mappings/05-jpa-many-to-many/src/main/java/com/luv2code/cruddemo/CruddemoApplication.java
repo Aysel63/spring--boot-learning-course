67ddf89bd61292ed2d5690d2038e799a232943fc
user <aysel.abdulzade05@gmail.com>
2026-04-15 15:54:38 +0400

    feat: create Student entity and map Many-to-Many relationship with Course

--- 09-spring-boot-jpa-advanced-mappings/05-jpa-many-to-many/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 09-spring-boot-jpa-advanced-mappings/05-jpa-many-to-many/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -23,10 +23,6 @@ public class CruddemoApplication {
     public CommandLineRunner commandLineRunner(AppDAO appDAO){
         return runner-> {
 
-            //  createCourseAndReviews(appDAO);
-           // retrieveCourseAndReviews(appDAO);
-            deleteCourseAndReviews(appDAO);
-
         };
 
     }
